cd573967b14b01ef30b0a25e2c9f38b19391accb
Gabriel Raymundo <bieelvii@gmail.com>
2020-07-09 19:14:41 -0300

    arrumei o select do banco de dados

--- teste/admPage.php
+++ teste/admPage.php
@@ -9,20 +9,37 @@
 	 }
 ?>
 	
-	<div class="Secao">
+	<div class="Content">
+		<table class="TabelaSelecao">
+			<tr>
+				<td>Codígo</td>
+				<td>Usuário</td>
+				<td>E-mail</td>
+				<td>Ações</td>
+			</tr>
 		<?php 
 
-			$sqlSelecao = $conn->prepare("SELECT * FROM usuario");
+			$sqlSelecao = $conn->prepare("SELECT * FROM usuario ORDER BY codigoUsuario DESC");
 
 			$sqlSelecao->execute();
 
 			$resultado = $sqlSelecao->fetchAll(PDO::FETCH_ASSOC);
 
-			foreach ($resultado as $key => $value){
-				echo $key.": ".$value;
-				echo "<br>";
-			}
-		?>
-	</div>
+			foreach($resultado as $cadaItem){ ?>
+				<tr>
+					<td><?php echo $cadaItem["codigoUsuario"]; ?></td>
+					<td><?php echo $cadaItem["nomeUsuario"]; ?></td>
+					<td><?php echo $cadaItem["emailUsuario"]; ?></td>
+					<td>
+						<a href="visualiza.php"><img src="image/lupa.png" width="30" height="30"></a>
+						<a href="atualiza.php"><img src="image/lapis.png" width="30" height="30"></a>
+						<a href="deleta.php"><img src="image/lixeira.png" width="30" height="30"></a>
+					</td>
+				</tr>			
 
+			<?php 
+			} 
+			?>
+		</table>
+	</div>
 <?php include("footer.php"); ?>
