2a83ff5734bddd69e394be1ea84fbf151ba1d098
Gabriel Raymundo <bieelvii@gmail.com>
2020-07-08 23:40:21 -0300

    iniciado a parte de selecao do usuario adm

--- teste/admPage.php
+++ teste/admPage.php
@@ -1,21 +1,28 @@
 <?php session_start(); ?>
+<?php include("model/ConexaoDataBase.php"); ?>
 <?php include("header.php"); ?>
 
-<?php 
-	 
-	 if(isset($_SESSION['nomeUsuario'])){
-
-	 } else {
-
+<?php 	 
+	 if(!isset($_SESSION['nomeUsuario'])){
 	 	header('Location: http://localhost/teste/login.php');
 	 	exit;
-
 	 }
-
 ?>
+	
+	<div class="Secao">
+		<?php 
 
-<a href="sair.php">SAIR</a>
+			$sqlSelecao = $conn->prepare("SELECT * FROM usuario");
 
-<?php echo $_SESSION['nomeUsuario']; ?>
+			$sqlSelecao->execute();
+
+			$resultado = $sqlSelecao->fetchAll(PDO::FETCH_ASSOC);
+
+			foreach ($resultado as $key => $value){
+				echo $key.": ".$value;
+				echo "<br>";
+			}
+		?>
+	</div>
 
 <?php include("footer.php"); ?>
---
+++ teste/controller/selecaoController.php
@@ -0,0 +1,13 @@
+<?php
+	
+			$sqlSelecao = $conn->prepare("SELECT * FROM usuario WHERE codigoUsuario = ?");
+
+			$sqlSelecao->bindValue(1, 79);
+
+			$sqlSelecao->execute();
+
+			$resultado = $sqlSelecao->fetch();
+
+			print_r($resultado);
+
+ ?>
